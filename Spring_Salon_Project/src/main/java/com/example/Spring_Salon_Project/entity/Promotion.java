78e828e8cb166165c8c9c56e07c8748ea3f35a24
Dilni Sehansa <dilnisehansa29@gmail.com>
2026-08-12 20:18:53 +0530

    promotion table

--- Spring_Salon_Project/src/main/java/com/example/Spring_Salon_Project/entity/Promotion.java
+++ Spring_Salon_Project/src/main/java/com/example/Spring_Salon_Project/entity/Promotion.java
@@ -1,4 +1,30 @@
 package com.example.Spring_Salon_Project.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "promotions")
 public class Promotion {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long promotionId;
+
+    @Column(unique = true, nullable = false)
+    private String promoCode;
+
+    private String description;
+    private Double discount;
+    private LocalDate startDate;
+    private LocalDate endDate;
+
 }
