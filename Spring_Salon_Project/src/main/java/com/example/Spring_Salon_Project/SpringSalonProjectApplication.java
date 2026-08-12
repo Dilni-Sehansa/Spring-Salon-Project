5ee0de53b746c5557deb3c3cfd67dd3262486915
Dilni Sehansa <dilnisehansa29@gmail.com>
2026-08-12 12:47:33 +0530

    Initial project setup

---
+++ Spring_Salon_Project/src/main/java/com/example/Spring_Salon_Project/SpringSalonProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.Spring_Salon_Project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSalonProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSalonProjectApplication.class, args);
+	}
+
+}
