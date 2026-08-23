1e3ddfe37a9434f62f90457f5e9f62b468d8d1f6
Dilni Sehansa <dilnisehansa29@gmail.com>
2026-08-23 10:32:23 +0530

    staff entity and UI

--- Spring_Salon_Project/src/main/java/com/example/Spring_Salon_Project/entity/Staff.java
+++ Spring_Salon_Project/src/main/java/com/example/Spring_Salon_Project/entity/Staff.java
@@ -1,4 +1,37 @@
 package com.example.Spring_Salon_Project.entity;
 
+import com.example.Spring_Salon_Project.enumiration.CustomerStatus;
+import com.example.Spring_Salon_Project.enumiration.StaffStatus;
+import jakarta.persistence.*;
+import lombok.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "staff")
 public class Staff {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false)
+    private Long staffId;
+
+    @OneToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Column(nullable = false)
+    private String specialization;
+
+    @Column(nullable = false)
+    private String bio;
+
+    private Integer experienceYears;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private StaffStatus staffStatus;
+    
+
 }
---
+++ Spring_Salon_Project/src/main/java/com/example/Spring_Salon_Project/enumiration/StaffStatus.java
@@ -0,0 +1,7 @@
+package com.example.Spring_Salon_Project.enumiration;
+
+public enum StaffStatus {
+    AVAILABLE,
+    ON_LEAVE,
+    INACTIVE
+}
