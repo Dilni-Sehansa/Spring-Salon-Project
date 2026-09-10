d46ed6d7abfdf930b1d58e45f0f1a0f132c006bc
Dilni Sehansa <dilnisehansa29@gmail.com>
2026-09-10 11:51:40 +0530

    update UI design

--- Spring_Salon_Project/src/main/java/com/example/Spring_Salon_Project/security/SecurityConfig.java
+++ Spring_Salon_Project/src/main/java/com/example/Spring_Salon_Project/security/SecurityConfig.java
@@ -51,36 +51,134 @@ public class SecurityConfig {
 //                                .requestMatchers("/v1/user/**").permitAll()
 //                )
                 .authorizeHttpRequests(auth -> auth
+
                         .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
                         .requestMatchers("/", "/*.html", "/css/**", "/js/**", "/images/**", "/uploads/**").permitAll()
                         .requestMatchers("/login.html", "/singIn.html", "/dashboard.html", "/user.html").permitAll()
 
+                        //-------------------------USER----------------------------------------
                         .requestMatchers(HttpMethod.POST, "/v1/user/login").permitAll()
                         .requestMatchers(HttpMethod.POST, "/v1/user/user_saved").permitAll()
+
+                        //-------------------- Authenticated user endpoints ------------------------
                         .requestMatchers("/v1/user/**").authenticated()
-
-                        .requestMatchers("/v1/customer/**").authenticated()
-                        .requestMatchers("/v1/staff/**").authenticated()
-
-                        .requestMatchers(HttpMethod.GET, "/v1/category/**").permitAll()
-                        .requestMatchers("/v1/category/**").hasAnyRole("ADMIN", "STAFF")
-
-                        .requestMatchers(HttpMethod.GET, "/v1/service/**").permitAll()
-                        .requestMatchers("/v1/service/**").hasAnyRole("ADMIN", "STAFF")
-
-                        .requestMatchers(HttpMethod.GET, "/v1/staffService/**").permitAll()
-                        .requestMatchers("/v1/staffService/**").hasAnyRole("ADMIN", "STAFF")
-
-//                        .requestMatchers(HttpMethod.GET, "/v1/product/**").permitAll()
-                        .requestMatchers("/v1/product/**").hasAnyRole("ADMIN", "STAFF")
-
-                        .requestMatchers(HttpMethod.POST, "/v1/appointment/**").hasAnyRole("CUSTOMER", "ADMIN", "STAFF")
-                        .requestMatchers(HttpMethod.GET, "/v1/appointment/**").hasAnyRole("CUSTOMER", "ADMIN", "STAFF")
-                        .requestMatchers(HttpMethod.DELETE, "/v1/appointment/**").hasAnyRole("ADMIN", "STAFF")
-                        .requestMatchers(HttpMethod.PATCH, "/v1/appointment/**").hasAnyRole("ADMIN", "STAFF")
-
                         .requestMatchers("/v1/notification/**").authenticated()
 
+                        //--------------------Public read endpoints----------------------------------
+                        .requestMatchers(HttpMethod.GET, "/v1/category/**").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/v1/service/**").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/v1/staffService/**").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/v1/staffSchedule/**").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/v1/feedback/**").permitAll()
+
+                        // -------------------- Customer + Staff + Admin ---------------------------------
+                        .requestMatchers(HttpMethod.GET,  "/v1/customer/**").hasAnyRole("CUSTOMER", "ADMIN", "STAFF")
+                        .requestMatchers(HttpMethod.PUT,  "/v1/customer/**").hasAnyRole("CUSTOMER", "ADMIN", "STAFF")
+                        .requestMatchers(HttpMethod.GET,  "/v1/product/**").hasAnyRole("CUSTOMER", "ADMIN", "STAFF")
+                        .requestMatchers(HttpMethod.GET,  "/v1/payment/**").hasAnyRole("CUSTOMER", "ADMIN", "STAFF")
+                        .requestMatchers(HttpMethod.POST, "/v1/appointment/**").hasAnyRole("CUSTOMER", "ADMIN", "STAFF")
+                        .requestMatchers(HttpMethod.GET,  "/v1/appointment/**").hasAnyRole("CUSTOMER", "ADMIN", "STAFF")
+                        .requestMatchers(HttpMethod.GET,  "/v1/appointmentDetail/**").hasAnyRole("CUSTOMER", "ADMIN", "STAFF")
+                        .requestMatchers(HttpMethod.POST, "/v1/feedback/**").hasAnyRole("CUSTOMER", "ADMIN")
+
+
+                        // ---------------------------- Staff + Admin --------------------------------------------
+                        .requestMatchers("/v1/customer/**").hasAnyRole("ADMIN", "STAFF")
+                        .requestMatchers("/v1/product/**").hasAnyRole("ADMIN", "STAFF")
+                        .requestMatchers("/v1/payment/**").hasAnyRole("ADMIN", "STAFF")
+                        .requestMatchers(HttpMethod.DELETE, "/v1/appointment/**").hasAnyRole("ADMIN", "STAFF")
+                        .requestMatchers(HttpMethod.PATCH,  "/v1/appointment/**").hasAnyRole("ADMIN", "STAFF")
+                        .requestMatchers("/v1/appointmentDetail/**").hasAnyRole("ADMIN", "STAFF")
+                        .requestMatchers("/v1/staffService/**").hasAnyRole("ADMIN", "STAFF")
+                        .requestMatchers(HttpMethod.GET, "/v1/staff/**").hasAnyRole("ADMIN", "STAFF")
+                        .requestMatchers(HttpMethod.GET, "/v1/supplier/**").hasAnyRole("ADMIN", "STAFF")
+
+                        //----------------------------------- Admin only -----------------------------------------------
+                        .requestMatchers("/v1/category/**").hasRole("ADMIN")
+                        .requestMatchers("/v1/service/**").hasRole("ADMIN")
+                        .requestMatchers("/v1/staffSchedule/**").hasRole("ADMIN")
+                        .requestMatchers("/v1/staff/**").hasRole("ADMIN")
+                        .requestMatchers("/v1/feedback/**").hasRole("ADMIN")
+                        .requestMatchers("/v1/auditLog/**").hasRole("ADMIN")
+                        .requestMatchers("/v1/supplier/**").hasRole("ADMIN")
+
+//                        //-------------------------CUSTOMER----------------------------------------
+//                        .requestMatchers(HttpMethod.GET, "/v1/customer/**").hasAnyRole("CUSTOMER", "ADMIN", "STAFF")
+//                        .requestMatchers(HttpMethod.PUT, "/v1/customer/**").hasAnyRole("CUSTOMER", "ADMIN", "STAFF")
+//                        .requestMatchers("/v1/customer/**").hasAnyRole("ADMIN", "STAFF")
+//
+//                         //-------------------------CATEGORY----------------------------------------
+//                        .requestMatchers(HttpMethod.GET, "/v1/category/**").permitAll()
+//                        .requestMatchers("/v1/category/**").hasAnyRole("ADMIN", "STAFF")
+//
+//                                //-------------------------SERVICE----------------------------------------
+//                        .requestMatchers(HttpMethod.GET, "/v1/service/**").permitAll()
+//                        .requestMatchers("/v1/service/**").hasAnyRole("ADMIN", "STAFF")
+//
+//                                //-------------------------STAFF SERVICE----------------------------------------
+//                        .requestMatchers(HttpMethod.GET, "/v1/staffService/**").permitAll()
+//                        .requestMatchers("/v1/staffService/**").hasAnyRole("ADMIN", "STAFF")
+//
+//                                //-------------------------STAFF SCHEDULE----------------------------------------
+//                        .requestMatchers(HttpMethod.GET, "/v1/staffSchedule/**").permitAll()
+//                        .requestMatchers("/v1/staffSchedule/**").hasAnyRole("ADMIN")
+//
+//
+//
+//                                //-------------------------SERVICE----------------------------------------
+//                        .requestMatchers(HttpMethod.GET, "/v1/service/**").permitAll()
+//                        .requestMatchers(HttpMethod.POST, "/v1/service/**").hasAnyRole("ADMIN", "STAFF")
+//                        .requestMatchers(HttpMethod.PUT, "/v1/service/**").hasAnyRole("ADMIN", "STAFF")
+//                        .requestMatchers(HttpMethod.PATCH, "/v1/service/**").hasAnyRole("ADMIN", "STAFF")
+//                        .requestMatchers("/v1/service/**").hasAnyRole("ADMIN")
+//
+//                                //-------------------------CATEGORY----------------------------------------
+//                        .requestMatchers(HttpMethod.GET, "/v1/category/**").permitAll()
+//                        .requestMatchers(HttpMethod.POST, "/v1/category/**").hasAnyRole("ADMIN", "STAFF")
+//                        .requestMatchers(HttpMethod.PUT, "/v1/category/**").hasAnyRole("ADMIN", "STAFF")
+//                        .requestMatchers(HttpMethod.PATCH, "/v1/category/**").hasAnyRole("ADMIN", "STAFF")
+//                        .requestMatchers("/v1/category/**").hasAnyRole("ADMIN")
+//
+//                        //-------------------------PRODUCT----------------------------------------
+//                        .requestMatchers(HttpMethod.GET, "/v1/product/**").hasAnyRole("CUSTOMER", "ADMIN", "STAFF")
+//                        .requestMatchers("/v1/product/**").hasAnyRole("ADMIN", "STAFF")
+//
+//                        //-------------------------PAYMENT----------------------------------------
+//                        .requestMatchers(HttpMethod.GET, "/v1/payment/**").hasAnyRole("CUSTOMER", "ADMIN", "STAFF")
+//                        .requestMatchers("/v1/payment/**").hasAnyRole("ADMIN", "STAFF")
+//
+//                        //-------------------------APPOINTMENT----------------------------------------
+//                        .requestMatchers(HttpMethod.POST, "/v1/appointment/**").hasAnyRole("CUSTOMER", "ADMIN", "STAFF")
+//                        .requestMatchers(HttpMethod.GET, "/v1/appointment/**").hasAnyRole("CUSTOMER", "ADMIN", "STAFF")
+//                        .requestMatchers(HttpMethod.DELETE, "/v1/appointment/**").hasAnyRole("ADMIN", "STAFF")
+//                        .requestMatchers(HttpMethod.PATCH, "/v1/appointment/**").hasAnyRole("ADMIN", "STAFF")
+//
+//                        //-------------------------APPOINTMENT DETAILS----------------------------------------
+//                        .requestMatchers(HttpMethod.GET, "/v1/appointmentDetail/**").hasAnyRole("CUSTOMER", "ADMIN", "STAFF")
+//                        .requestMatchers("/v1/appointmentDetail/**").hasAnyRole("ADMIN", "STAFF")
+//
+//                        //-------------------------NOTIFICATION----------------------------------------
+//                        .requestMatchers("/v1/notification/**").authenticated()
+//
+//                        //-------------------------STAFF----------------------------------------
+//                        .requestMatchers(HttpMethod.GET, "/v1/staff/**").hasAnyRole("ADMIN", "STAFF")
+//                        .requestMatchers("/v1/staff/**").hasRole("ADMIN")
+//
+//
+//                        //-------------------------FEEDBACK----------------------------------------
+//                        .requestMatchers(HttpMethod.GET, "/v1/feedback/**").permitAll()
+//                        .requestMatchers(HttpMethod.POST, "/v1/feedback/**").hasAnyRole("ADMIN", "CUSTOMER")
+//                        .requestMatchers("/v1/feedback/**").hasAnyRole("ADMIN")
+//
+//                        //-------------------------NOTIFICATION----------------------------------------
+//                        .requestMatchers("/v1/notification/**").authenticated()
+//
+//                        //-------------------------AUDIT LOG----------------------------------------
+//                        .requestMatchers("/v1/auditLog/**").hasRole("ADMIN")
+//
+//                        //-------------------------SUPPLIER----------------------------------------
+//                        .requestMatchers(HttpMethod.GET, "/v1/supplier/**").hasAnyRole("ADMIN", "STAFF")
+//                        .requestMatchers("/v1/supplier/**").hasRole("ADMIN")
 
                         .anyRequest().authenticated()
                 )
